a0085bed04cf80b79fc7b501b0b37bd0f45720dd
kitagawa-hr <kitagawa@cancerscan.jp>
2021-05-18 16:58:13 +0900

    Add main script

---
+++ main.py
@@ -0,0 +1,113 @@
+import re
+
+
+class BaseJapaneseNumeralsException(Exception):
+    pass
+
+
+class ValidationError(BaseJapaneseNumeralsException):
+    pass
+
+
+class ParseError(BaseJapaneseNumeralsException):
+    pass
+
+
+class NotSupportedError(BaseJapaneseNumeralsException):
+    pass
+
+
+UNITS = [
+    ("兆", 10 ** 12),
+    ("億", 10 ** 8),
+    ("万", 10 ** 4),
+    ("千", 10 ** 3),
+    ("百", 10 ** 2),
+    ("十", 10),
+]
+
+JAPANESE_DIGITS = {
+    "〇": 0,
+    "一": 1,
+    "二": 2,
+    "三": 3,
+    "四": 4,
+    "五": 5,
+    "六": 6,
+    "七": 7,
+    "八": 8,
+    "九": 9,
+}
+
+
+DAIJI_TRANS = str.maketrans(
+    {
+        "零": "〇",
+        "壱": "一",
+        "弐": "二",
+        "参": "三",
+        "拾": "十",
+    }
+)
+
+UNIT_CHARS = "".join(u for (u, _) in UNITS)
+JAPANESE_DIGIT_CHARS = "".join(JAPANESE_DIGITS)
+SUPPORTED_CHARS = UNIT_CHARS + JAPANESE_DIGIT_CHARS
+SUPPORTED_REGEX = re.compile(rf"^[{SUPPORTED_CHARS}]+$")
+
+
+def validate(s: str):
+    if s == "":
+        raise ValidationError("Empty string")
+    if re.search(r"一[百十]", s):
+        raise ValidationError("'一百' and '一十' are invalid in common writing. 100 is just '百', and 10 is just '十'")
+    if re.search(rf"([^{JAPANESE_DIGIT_CHARS}]|^)千[万億兆]", s):
+        raise ValidationError(
+            "if '千' directly precedes the name of powers of myriad, '一' is normally attached before '千'"
+        )
+    if re.search(rf"[{JAPANESE_DIGIT_CHARS}]{{2,}}[十百千]", s):
+        raise ValidationError("Each place (十,百,千) should not have more than one digit")
+    if re.search(r"([万億兆]{2,}|^[万億兆])", s):
+        raise ValidationError("万,億,兆 cannot be adjacent to each other or be the first character of the sequence")
+
+
+def parse(s: str, index: int = 0) -> int:
+    if len(s) == 0:
+        return 0
+    if index >= len(UNITS):
+        if s.startswith("〇"):
+            raise ParseError("Positional notation or place of '一' should not start with zero")
+        if not re.match(rf"^[{JAPANESE_DIGIT_CHARS}]+$", s):
+            raise ParseError("throws Error when name of powers of 10 appears more than once in subsequence")
+        return int("".join(str(JAPANESE_DIGITS[c]) for c in s))
+    (unit_str, unit_number) = UNITS[index]
+    splitted = s.split(unit_str)
+    if len(splitted) == 1:
+        # unit_str not found
+        return parse(splitted[0], index + 1)
+    if len(splitted) == 2:
+        # one unit_str found
+        if splitted[0] == "":
+            return unit_number + parse(splitted[1], index + 1)
+        if unit_str in {"兆", "億", "万"}:
+            # units over 千 is forbid in the left of (兆, 億, 万)
+            return unit_number * parse(splitted[0], 3) + parse(splitted[1], index + 1)
+        return unit_number * parse(splitted[0], len(UNITS)) + parse(splitted[1], index + 1)
+    raise ParseError(f"{unit_str} appears more than once in subsequence {s}")
+
+
+def to_number(s: str, validation: bool = True, accept_daiji: bool = True):
+    if not isinstance(s, str):
+        raise TypeError(f"{s} is not string")
+    if len(s) == 1 and s == "〇":
+        # treat zero as special case
+        return 0
+    if len(s) > 32:
+        raise NotSupportedError(f"{s} is too long.")
+    if validation:
+        validate(s)
+    if accept_daiji:
+        s = s.translate(DAIJI_TRANS)
+    if not SUPPORTED_REGEX.match(s):
+        raise NotSupportedError(f"{s} contains unsupported characters. Supported format: {SUPPORTED_REGEX.pattern}")
+    return parse(s)
---
+++ test_main.py
@@ -0,0 +1,119 @@
+import pytest
+
+from main import NotSupportedError, ParseError, ValidationError, to_number
+
+
+@pytest.mark.parametrize(
+    "input_case, expected",
+    [
+        ("〇", 0),
+        ("三〇", 30),
+        ("六八", 68),
+        ("一六〇〇", 1600),
+        ("二〇一七", 2017),
+        ("二十", 20),
+        ("五十九", 59),
+        ("三百二", 302),
+        ("四千五百", 4500),
+        ("十", 10),
+        ("十五", 15),
+        ("百", 100),
+        ("百十", 110),
+        ("百八", 108),
+        ("千六", 1006),
+        ("千十", 1010),
+        ("千百十", 1110),
+        ("百万", 1000000),
+        ("一万五千八百", 15800),
+        ("二億五千万", 250000000),
+        ("十兆百億七百万", 10010007000000),
+        ("百兆三", 100000000000003),
+        ("二千万", 20000000),
+        ("一千万", 10000000),
+        ("千五百", 1500),
+        ("一千五百", 1500),
+        ("千五百万", 15000000),
+        ("一千五百万", 15000000),
+        ("参弐壱", 321),
+        ("弐拾参", 23),
+        ("拾弐万参千弐百拾壱", 123211),
+        ("九千七兆千九百九十二億五千四百七十四万九百九十一", 9007199254740991),
+        ("九〇〇七兆一九九二億五四七四万九九一", 9007199254740991),
+    ],
+)
+def test_valid_cases(input_case, expected):
+    assert to_number(input_case) == expected
+
+
+@pytest.mark.parametrize(
+    "input_case",
+    [
+        # throws Error when input contains unsupported characters
+        ("第百十六回"),
+        # throws Error when input string is too long
+        ("一二三四五六七八九〇一二三四五六七八九〇一二三四五六七八九〇一二三"),
+    ],
+)
+def test_raises_not_supported_error(input_case):
+    with pytest.raises(NotSupportedError):
+        to_number(input_case)
+
+
+@pytest.mark.parametrize(
+    "input_case",
+    [
+        # throws Error when input string is empty
+        (""),
+        # "一百" and "一十" are invalid in common writing. 100 is just "百", and 10 is just "十"
+        ("一十"),
+        ("一百"),
+        ("一百一十"),
+        ("四百一十五"),
+        # if "千" directly precedes the name of powers of myriad, "一" is normally attached before "千"
+        ("千万"),
+        ("一億千万"),
+        ("億千万"),
+        # Each place (十,百,千,拾) should not have more than one digit
+        ("六七百"),
+        ("二〇〇十七"),
+        ("三四千五六七百"),
+        # 万,億,兆 cannot be adjacent to each other or be the first character of the sequence
+        ("万"),
+        ("億"),
+        ("兆"),
+        ("兆億万"),
+    ],
+)
+def test_raises_validation_error(input_case):
+    with pytest.raises(ValidationError):
+        to_number(input_case)
+
+
+@pytest.mark.parametrize(
+    "input_case",
+    [
+        # throws Error when name of powers of 10 appears more than once in subsequence
+        ("十百千"),
+        ("三十七百九千"),
+        ("四万三億二兆"),
+        # Each place (十,百,千,拾) should not start with zero
+        ("〇十"),
+        ("〇千〇百〇十"),
+        # Positional notation or place of "一" should not start with zero
+        ("〇〇"),
+        ("〇十〇"),
+        ("〇一二三"),
+        ("十〇"),
+        ("千〇"),
+        ("千〇〇"),
+        ("〇万"),
+        # throws Error when name of powers of 10 appears more than once in subsequence
+        ("十十"),
+        ("二十五十"),
+        ("三十二十"),
+        ("八百五百"),
+    ],
+)
+def test_raises_parse_error(input_case):
+    with pytest.raises(ParseError):
+        to_number(input_case)
